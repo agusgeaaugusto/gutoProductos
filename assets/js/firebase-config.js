2347dc4537028ec72a389f2988e166000d01c5d8
agusgeaaugusto <61166590+agusgeaaugusto@users.noreply.github.com>
2025-10-30 10:29:39 -0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,8 @@
-// assets/js/firebase-config.js
 export const firebaseConfig = {
   apiKey: "AIzaSyD-dvFNkQdS_mGlUeA6Dh-PprkriblHNhE",
   authDomain: "gutoproducto.firebaseapp.com",
   projectId: "gutoproducto",
-  // OJO: si Storage te da error, cambiá esto a "gutoproducto.appspot.com"
-  storageBucket: "gutoproducto.firebasestorage.app",
+  storageBucket: "gutoproducto.appspot.com",   // <- MUY IMPORTANTE
   messagingSenderId: "827696843285",
   appId: "1:827696843285:web:2448d558f743a626529c1c",
   measurementId: "G-6E2K2CK5L5"
